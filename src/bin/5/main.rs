515da664a45d24cc223b45a54046524ac5ec85ab
Brandon Doornbos <41441504+brandon-doornbos@users.noreply.github.com>
2023-12-05 18:16:15 +0100

    Optimize day 5
    Did the math on ranges as I mentioned in the previous commit.
    So whenever a range crosses a translation's boundary, we create a
    new range. We're not combining them, so on an infinitely large
    problem we would get progressively slower. However for this input it's
    been reduced from 83 seconds to 0.5 milliseconds.
    
    We're still checking all transition ranges for every number, since sorting
    each and looping through them only when it was within bounds turned out
    to be slower.

--- src/bin/5/main.rs
+++ src/bin/5/main.rs
@@ -1,7 +1,11 @@
-#[derive(Debug)]
 struct Translation {
     source: i64,
     destination: i64,
+    length: i64,
+}
+
+struct Range {
+    start: i64,
     length: i64,
 }
 
@@ -25,14 +29,13 @@ fn preprocess(input: &std::str::Lines) -> (Vec<i64>, Vec<Vec<Translation>>) {
         .map(|x| x.parse().unwrap())
         .collect();
 
-    let mut translations = vec![vec![]];
+    iter.next();
+    iter.next();
+
+    let mut translations: Vec<Vec<Translation>> = vec![vec![]];
 
     for line in iter {
-        if line == "" {
-            continue;
-        } else if line.starts_with(|c: char| c.is_ascii_alphabetic()) {
-            translations.push(vec![]);
-        } else {
+        if line.starts_with(|c: char| c.is_ascii_digit()) {
             let translation: Vec<i64> = line
                 .split_whitespace()
                 .map(|x| x.parse().unwrap())
@@ -44,13 +47,15 @@ fn preprocess(input: &std::str::Lines) -> (Vec<i64>, Vec<Vec<Translation>>) {
                 source: translation[1],
                 length: translation[2],
             })
+        } else if line == "" {
+            translations.push(vec![]);
         }
     }
 
     (seeds, translations)
 }
 
-fn process_translations(numbers: &mut Vec<i64>, translations: &Vec<Translation>) {
+fn process_translations_1(numbers: &mut Vec<i64>, translations: &Vec<Translation>) {
     for number in numbers {
         for translation in translations {
             if *number >= translation.source && *number < translation.source + translation.length {
@@ -64,25 +69,61 @@ fn process_translations(numbers: &mut Vec<i64>, translations: &Vec<Translation>)
 fn part_1(seeds: &Vec<i64>, translations: &Vec<Vec<Translation>>) -> i64 {
     let mut results = seeds.clone();
     for layer in translations {
-        process_translations(&mut results, layer);
+        process_translations_1(&mut results, layer);
     }
 
     *results.iter().min().unwrap()
 }
 
-fn part_2(seed_ranges: &Vec<i64>, translations: &Vec<Vec<Translation>>) -> i64 {
-    let mut seeds = vec![];
-    for i in (0..seed_ranges.len()).step_by(2) {
-        for j in seed_ranges[i]..(seed_ranges[i] + seed_ranges[i + 1]) {
-            seeds.push(j);
+fn process_translations_2(ranges: &mut Vec<Range>, translations: &Vec<Translation>) {
+    let mut result = vec![];
+
+    while !ranges.is_empty() {
+        for i in (0..ranges.len()).rev() {
+            let mut not_removed = true;
+
+            for translation in translations {
+                let translation_end = translation.source + translation.length;
+
+                if ranges[i].start >= translation.source && ranges[i].start < translation_end {
+                    let range_end = ranges[i].start + ranges[i].length;
+
+                    if range_end > translation_end {
+                        ranges.push(Range {
+                            start: translation_end,
+                            length: range_end - translation_end,
+                        });
+                        ranges[i].length = translation_end - ranges[i].start;
+                    }
+
+                    ranges[i].start += translation.destination - translation.source;
+                    result.push(ranges.swap_remove(i));
+                    not_removed = false;
+                    break;
+                }
+            }
+            if not_removed {
+                result.push(ranges.swap_remove(i));
+            }
         }
     }
 
-    println!("{:?}", seeds.len());
+    ranges.append(&mut result);
+}
 
-    for layer in translations {
-        process_translations(&mut seeds, layer);
+fn part_2(seeds: &Vec<i64>, translations: &Vec<Vec<Translation>>) -> i64 {
+    let mut seed_ranges = vec![];
+    for i in (0..seeds.len()).step_by(2) {
+        seed_ranges.push(Range {
+            start: seeds[i],
+            length: seeds[i + 1],
+        });
     }
 
-    *seeds.iter().min().unwrap()
+    for layer in translations {
+        process_translations_2(&mut seed_ranges, layer);
+    }
+
+    seed_ranges.sort_unstable_by(|a, b| a.start.cmp(&b.start));
+    seed_ranges[0].start
 }
